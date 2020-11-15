709dd708ed262018de198b9a3102ac12ce970526
TimTran-Dev <timv.tran.dev@gmail.com>
2020-11-15 14:21:30 -0500

    preFizzBuzz

---
+++ preFizzBuzz.js
@@ -0,0 +1,11 @@
+// Pre-FizzBuzz Workout #1
+// https://www.codewars.com/kata/569e09850a8e371ab200000b/train/javascript
+// 8 Kyu
+
+function preFizz(n) {
+    const arr = []
+    for(let i = 1; i <= n; i++) {
+      arr.push(i)
+    }
+    return arr
+  }
