9e3708a89865f519e357e45f159e23a8f546d14b
TimTran-Dev <timv.tran.dev@gmail.com>
2020-11-16 14:23:37 -0500

    enumerableMagic

---
+++ enumerableMagic.js
@@ -0,0 +1,7 @@
+// Enumerable Magic #25 - Take the first N Elements
+// https://www.codewars.com/kata/545afd0761aa4c3055001386/train/javascript
+// 8 Kyu
+
+function take(arr, n) {
+    return arr.slice(0, n)
+  }
