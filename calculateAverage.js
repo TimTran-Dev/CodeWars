78ccad18d0239936760e31e8a11604ebb691ded0
TimTran-Dev <timv.tran.dev@gmail.com>
2020-11-25 14:17:41 -0500

    calculateAverage

---
+++ calculateAverage.js
@@ -0,0 +1,7 @@
+// Calculate average
+// https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/javascript
+// 8 Kyu
+
+function find_average(array) {
+    return array.reduce((a, b) => a + b) / array.length
+  }
