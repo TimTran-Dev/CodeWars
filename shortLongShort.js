6e6a08d0505488fe24e06b8a2db7f2c8bb1800a9
TimTran-Dev <timv.tran.dev@gmail.com>
2020-11-28 14:14:05 -0500

    shortLongShort

---
+++ shortLongShort.js
@@ -0,0 +1,9 @@
+// Short Long Short
+// https://www.codewars.com/kata/50654ddff44f800200000007/train/javascript
+// 8 Kyu
+
+
+function solution(a, b){
+    return a.length > b.length ? b + a + b : a + b + a
+  }
+  
