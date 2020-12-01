0865412403077f9b6b3e9b004a69d1e5bae5c9df
TimTran-Dev <>
2020-12-01 11:52:33 -0500

    English

---
+++ english.js
@@ -0,0 +1,6 @@
+//Do you speak "English"?
+//https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058/train/javascript
+
+function spEng(sentence){
+    return sentence.toLowerCase().includes('english')
+  }
