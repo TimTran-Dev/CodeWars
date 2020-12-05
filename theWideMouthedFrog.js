910afc9011514f66028a68a449c0898265a0885e
TimTran-Dev <timv.tran.dev@gmail.com>
2020-12-05 13:43:37 -0500

    theWideMouthedFrog

---
+++ theWideMouthedFrog.js
@@ -0,0 +1,7 @@
+// The Wide-Mouthed frog!
+// https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89/train/javascript
+// 8 Kyu
+
+function mouthSize(animal) {
+    return animal.toLowerCase() === 'alligator' ? 'small' : 'wide'
+  }
