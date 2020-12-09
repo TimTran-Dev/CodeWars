a69c966336670ac117f30a4a69d67da583cf41fe
TimTran-Dev <timv.tran.dev@gmail.com>
2020-12-09 11:38:55 -0500

    countingSheep

---
+++ countingSheep.js
@@ -0,0 +1,8 @@
+// Counting sheep...
+// https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+// 8 Kyu
+
+
+function countSheeps(arrayOfSheep) {
+    return arrayOfSheep.filter(Boolean).length
+  }
