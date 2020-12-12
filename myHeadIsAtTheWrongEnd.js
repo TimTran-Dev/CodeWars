56e4dd83a37d03dcc3b6d9d52b9fd0422e618832
TimTran-Dev <timv.tran.dev@gmail.com>
2020-12-12 07:48:27 -0500

    myHeadIsAtTheWrongEnd

---
+++ myHeadIsAtTheWrongEnd.js
@@ -0,0 +1,7 @@
+// My head is at the wrong end!
+// https://www.codewars.com/kata/56f699cd9400f5b7d8000b55/train/javascript
+// 8 Kyu
+
+function fixTheMeerkat(arr) {
+    return arr.reverse()
+   }
