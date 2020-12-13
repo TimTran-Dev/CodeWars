5d8f9a2a8b6ee6f02daf9cc9c785d85c6510d74b
TimTran-Dev <timv.tran.dev@gmail.com>
2020-12-13 07:50:34 -0500

    mergeTwoSortedArraysIntoOne

---
+++ mergeTwoSortedArraysIntoOne.js
@@ -0,0 +1,7 @@
+// Merge two sorted arrays into one
+// https://www.codewars.com/kata/5899642f6e1b25935d000161/train/javascript
+// 8 Kyu
+
+function mergeArrays(arr1, arr2) {
+    return [...new Set(arr1.concat(arr2).sort((a, b) => a - b))]
+  }
