d54da4d387b476d08f7dfd039d52037aa0904d14
TimTran-Dev <timv.tran.dev@gmail.com>
2020-12-22 17:07:43 -0500

    removeFirstAndLastCharacter

---
+++ removeFirstAndLastCharacter.js
@@ -0,0 +1,7 @@
+// Remove First and Last Character
+// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
+// 8 Kyu
+
+function removeChar(str){
+    return str.slice(1, -1)
+   };   
