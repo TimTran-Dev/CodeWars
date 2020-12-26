6e370e43825d7cb11c07cac6418598644da8cbb9
TimTran-Dev <timv.tran.dev@gmail.com>
2020-12-26 15:07:16 -0500

    convertNumberToReversedArrayOfDigits

---
+++ convertNumberToReversedArrayOfDigits.js
@@ -0,0 +1,7 @@
+// Convert number to reversed array of digits
+// https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/javascript
+// 8 Kyu
+
+function digitize(n) {
+    return n.toString().split('').map(Number).reverse()
+  }
