549e7c5e68154613f7d847523fddf231f01f7e4b
TimTran-Dev <timv.tran.dev@gmail.com>
2020-12-29 15:12:22 -0500

    isNDivisibleByXAndY

---
+++ isNDivisibleByXAndY.js
@@ -0,0 +1,7 @@
+// Is n divisible by x and y?
+// https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
+// 8 Kyu
+
+function isDivisible(n, x, y) {
+    return n % x === 0 && n % y === 0
+}
