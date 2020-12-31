482396e614daeb400351aea34ef605972356c5f2
TimTran-Dev <timv.tran.dev@gmail.com>
2020-12-31 14:39:03 -0500

    grassHopperTerminalGameMoveFunction

---
+++ grassHopperTerminalGameMoveFunction.js
@@ -0,0 +1,7 @@
+// Grasshopper - Terminal game move function
+// https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/train/javascript
+// 8 Kyu
+
+function move (position, roll) {
+    return (roll * 2) + position
+  }
