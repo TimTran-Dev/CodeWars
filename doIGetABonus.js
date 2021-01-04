87627b637be58d9390b4df6ed9888b5c165ac659
TimTran-Dev <timv.tran.dev@gmail.com>
2021-01-04 14:46:00 -0500

    doIGetABonus

---
+++ doIGetABonus.js
@@ -0,0 +1,7 @@
+// Do I get a bonus?
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+// 8 Kyu
+
+function bonusTime(salary, bonus) {
+    return bonus ? `£${salary * 10}` : `£${salary}`
+  }
