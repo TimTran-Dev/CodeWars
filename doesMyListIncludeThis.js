7e9c11b093c1cbf6e81d6620496da48b98175b31
TimTran-Dev <timv.tran.dev@gmail.com>
2021-01-06 14:50:20 -0500

    doesMyListIncludeThis

---
+++ doesMyListIncludeThis.js
@@ -0,0 +1,7 @@
+// Enumerable Magic #3 - Does My List Include This?
+// https://www.codewars.com/kata/545991b4cbae2a5fda000158/train/javascript
+// 8 Kyu
+
+function include(arr, item){
+    return arr.includes(item)
+  }
