0128b891cff26cf960c70501c9f6e42a5ef1a0f2
TimTran-Dev <timv.tran.dev@gmail.com>
2021-01-08 14:53:50 -0500

    isThisMyTail

---
+++ isThisMyTail.js
@@ -0,0 +1,7 @@
+// Is this my tail?
+// https://www.codewars.com/kata/56f695399400f5d9ef000af5/train/javascript
+// 8 Kyu
+
+function correctTail(bod, tail) {
+    return bod.slice(-1) === tail
+  }  
