f503a85125706388d4cea9f6965d4e84aee9f7cd
TimTran-Dev <timv.tran.dev@gmail.com>
2021-01-09 14:55:33 -0500

    convertBooleanValuesToStringsYesOrNo

---
+++ convertBooleanValuesToStringsYesOrNo.js
@@ -0,0 +1,7 @@
+// Convert boolean values to strings 'Yes' or 'No'
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+// 8 Kyu
+
+function boolToWord( bool ){
+    return bool ? 'Yes' : 'No'
+  }
