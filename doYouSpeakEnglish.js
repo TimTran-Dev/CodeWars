95554aae5db29f5582192d9fa3e4e8a8e3dd32d3
TimTran-Dev <timv.tran.dev@gmail.com>
2021-01-10 14:57:10 -0500

    doYouSpeakEnglish

---
+++ doYouSpeakEnglish.js
@@ -0,0 +1,7 @@
+// Do you speak "English"?
+// https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058/train/javascript
+// 8 Kyu
+
+function spEng(sentence){
+    return sentence.toLowerCase().includes('english') 
+  }
