ff113f756868fb5ef59ff89aafa0cd9b9838f0cb
TimTran-Dev <timv.tran.dev@gmail.com>
2021-01-13 13:46:45 -0500

    parseNiceIntFromCharProblem

---
+++ parseNiceIntFromCharProblem.js
@@ -0,0 +1,8 @@
+// Parse nice int from char problem
+// https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/javascript
+// 8 Kyu
+
+function getAge(inputString){
+    const str = inputString.slice(0, 1)
+    return parseInt(str)
+  }
