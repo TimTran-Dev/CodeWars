89c0eb59f799fd19fa3c6e4c83063e2c20b8746a
TimTran-Dev <timv.tran.dev@gmail.com>
2021-01-24 12:34:44 -0500

    aStrangeTripToTheMarket

---
+++ aStrangeTripToTheMarket.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55ccdf1512938ce3ac000056/train/javascript
+// 8 Kyu
+// A Strange Trip to the Market
+
+
+function isLockNessMonster(s) {
+    return s.toLowerCase().includes('tree fiddy') || s.includes(3.50)
+}
