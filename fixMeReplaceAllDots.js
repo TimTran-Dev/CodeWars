1d13379c94093158ed2c29c488776db4d2623d88
TimTran-Dev <timv.tran.dev@gmail.com>
2021-01-28 13:41:04 -0500

    fixMeReplaceAllDots

---
+++ fixMeReplaceAllDots.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/596c6eb85b0f515834000049/train/javascript
+// FIXME: Replace all dots
+// 8 Kyu
+
+var replaceDots = function(str) {
+    return str.replace(/\./g, '-')
+}
