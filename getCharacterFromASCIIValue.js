fc587e3d6a5352bd92db6be0466c3c49894d2334
TimTran-Dev <timv.tran.dev@gmail.com>
2021-01-29 13:46:13 -0500

    getCharacterFromASCIIValue

---
+++ getCharacterFromASCIIValue.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/javascript
+// get character from ASCII Value
+// 8 Kyu
+
+function getChar(c) {
+    return String.fromCharCode(c)
+}
