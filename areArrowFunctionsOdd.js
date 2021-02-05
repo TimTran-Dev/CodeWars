84a631b6fba4c20260acc08e042f694dc0d8f0d0
TimTran-Dev <timv.tran.dev@gmail.com>
2021-02-05 12:16:57 -0500

    areArrowFunctionsOdd

---
+++ areArrowFunctionsOdd.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/559f80b87fa8512e3e0000f5/train/javascript
+// 8 Kyu
+// Are arrow functions odd?
+
+function odds(values) {
+    return values.filter((x) => x % 2)
+}
