a8cad725f0c1baab8cdbfab5ca74eef00a586c3f
TimTran-Dev <timv.tran.dev@gmail.com>
2021-02-23 12:45:05 -0500

    howManyLightsabersDoYouOwn

---
+++ howManyLightsabersDoYouOwn.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/51f9d93b4095e0a7200001b8/train/javascript
+// How many lightsabers do you own?
+// 8 Kyu
+
+function howManyLightsabersDoYouOwn(name) {
+    return name === 'Zach' ? 18 : 0
+}
