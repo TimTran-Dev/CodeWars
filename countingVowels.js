184c14a91baff502049d6af65351298333960b51
TimTran-Dev <timv.tran.dev@gmail.com>
2021-03-08 13:55:44 -0500

    countingVowels

---
+++ countingVowels.js
@@ -0,0 +1,14 @@
+function countingVowels(str) {
+    let count = 0
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count ++
+        }
+    }
+    return count
+}
+
+
+
+console.log(countingVowels('javascript')) // --> 3
