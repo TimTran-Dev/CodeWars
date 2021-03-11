bad143ace4d4cbcea840082c4723d7207c3866f1
TimTran-Dev <timv.tran.dev@gmail.com>
2021-03-11 13:02:44 -0500

    findMaxandMinValuesOfAList

---
+++ findMaxandMinValuesOfAList.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+// Find Maximum and Minimum Values of a List
+// 8 Kyu
+
+var min = function(list) {
+    return Math.min(...list)
+}
+var max = function(list) {
+    return Math.max(...list)
+}
