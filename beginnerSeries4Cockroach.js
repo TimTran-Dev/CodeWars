322818511f6f7e4573174d9f07b72669f03c4e20
TimTran-Dev <timv.tran.dev@gmail.com>
2021-03-13 13:13:00 -0500

    beginnerSeries4Cockroach

---
+++ beginnerSeries4Cockroach.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/javascript
+// Beginner Series #4 Cockroach
+// 8 Kyu
+
+function cockroachSpeed(s) {
+    let speed = Math.floor(s * 27.7781)
+    return speed
+}
