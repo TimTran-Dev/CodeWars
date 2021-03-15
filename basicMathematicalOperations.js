a60cea6981cff2a0d4d26b9a6f3b70b3f0c12051
TimTran-Dev <timv.tran.dev@gmail.com>
2021-03-15 13:46:45 -0400

    basicMathematicalOperations

---
+++ basicMathematicalOperations.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/javascript
+// Basic Mathematical Operations
+// 8 Kyu
+
+function basicOp(operation, value1, value2) {
+    switch(operation) {
+        case '+':
+            return value1 + value2
+            break;
+        case '-':
+            return value1 - value2
+            break;
+        case '*':
+            return value1 * value2
+            break;
+        case '/':
+            return value1 / value2
+            break;
+    }
+}
