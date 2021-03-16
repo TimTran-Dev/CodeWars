0f13417748201e6e25f8afbc32963197f84d7dbe
TimTran-Dev <timv.tran.dev@gmail.com>
2021-03-16 13:49:09 -0400

    findTheSmallestIntegerInTheArray

---
+++ findTheSmallestIntegerInTheArray.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/javascript
+// Find the smallest integer in the array
+// 8 Kyu
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return Math.min(...args)
+    }
+}
