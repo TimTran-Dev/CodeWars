38789d65cb87987c2d24d2c0e2fe3d2bb639e708
TimTran-Dev <timv.tran.dev@gmail.com>
2021-03-18 21:09:22 -0400

    isHeGonnaSurvive

---
+++ isHeGonnaSurvive.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+// Is he gonna survive?
+// 8 Kyu
+
+function hero(bullets, dragons) {
+    return bullets >= dragons * 2
+}
---
+++ trainingJs8ConditionalStatementSwitch.js
@@ -0,0 +1,46 @@
+// https://www.codewars.com/kata/572059afc2f4612825000d8a/train/javascript
+// Training JS #8: Conditional statement--switch
+// 8 Kyu
+
+function howManydays(month) {
+    var days;
+    switch(month) {
+        case 1:
+      return days = 31
+      break;
+    case 2:
+      return days = 28
+      break;
+    case 3:
+      return days = 31
+      break;
+    case 4:
+      return days = 30
+      break;
+    case 5:
+      return days = 31
+      break;
+    case 6: 
+      return days = 30
+      break;
+    case 7:
+      return days = 31
+      break;
+    case 8:
+      return days = 31
+      break;
+    case 9:
+      return days = 30
+      break;
+    case 10:
+      return days = 31
+      break;
+    case 11:
+      return days = 30
+      break;
+    case 12:
+      return days = 31
+      break;
+    }
+    return days
+}
