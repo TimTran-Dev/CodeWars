27eae2c3a74f6ccb373161fef89ac3c72c833f4f
TimTran-Dev <timv.tran.dev@gmail.com>
2021-03-25 10:46:01 -0400

    beConciseITheTernaryOperator

---
+++ beConciseITheTernaryOperator.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/56f3f6a82010832b02000f38/train/javascript
+// Be Concise 1 - The Ternary Operator
+// 8 Kyu
+
+
+const describeAge=a=>a<=12?"You're a(n) kid":a>=13&&a<=17?"You're a(n) teenager":a>=18&&a<=64?"You're a(n) adult":"You're a(n) elderly"
