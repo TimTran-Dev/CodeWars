612fc144bc65e767b93dcc99b87acfabc1f3804c
TimTran-Dev <timv.tran.dev@gmail.com>
2021-03-31 13:14:49 -0400

    trainingJs10

---
+++ trainingJs10.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5721a78c283129e416000999/train/javascript
+// Training JS #10: loop statement--for
+// 8 Kyu
+
+function pickIt(arr) {
+  var odd = [],
+    even = [];
+  //coding here
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
+  }
+
+  return [odd, even];
+}
