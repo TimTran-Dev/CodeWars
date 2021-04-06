419a2d79458dace3b9b4ba0c7894b8a70e4bd3dd
TimTran-Dev <timv.tran.dev@gmail.com>
2021-04-06 13:48:00 -0400

    findTheIntegral

---
+++ findTheIntegral.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/59811fd8a070625d4c000013/train/javascript
+// Find the integral
+// 8 Kyu
+
+function integrate(coefficient, exponent) {
+  return `${coefficient / (exponent + 1)}x^${exponent + 1}`;
+}
