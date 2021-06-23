29df95ba36f68ed696c1fedef2a84afe6912d1bc
TimTran-Dev <timv.tran.dev@gmail.com>
2021-06-23 16:18:34 -0400

    fizzBuzzLC

---
+++ fizzBuzzLC.js
@@ -0,0 +1,24 @@
+// Fizz Buzz
+// https://leetcode.com/problems/fizz-buzz/
+// Leetcode - Easy
+
+ 
+var fizzBuzz = function(n) {
+    let str = []
+    for (let i = 1; i <= n; i++) {
+        
+        if (i % 15 === 0) {
+            str.push('FizzBuzz')
+        }
+        else if (i % 3 === 0) {
+            str.push('Fizz')
+        }
+        else if (i % 5 === 0) {
+            str.push('Buzz')
+         } else {
+             str.push(i.toString())
+         }
+        
+    }
+    return str
+};
